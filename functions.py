5041a320dd142a308e967ec3a85f388b61c15371
Hetansh Mehta <43009373+Hetansh-Mehta@users.noreply.github.com>
2019-01-09 13:22:06 -0500

    Updated functions
    Removed syntax errors and power()

--- functions.py
+++ functions.py
@@ -14,13 +14,6 @@ def func2(arg1, arg2):
 def cube(x):
   return x*x*x
 
-# function with default value for an argument
-def power(num, x=1):
-  result = 1;
-  for i in range(x):
-    result = result * num  
-  return result
-
 #function with variable number of arguments
 def multi_add(*args):
   result = 0;
@@ -34,7 +27,4 @@ print (func1)
 func2(10,20)
 print (func2(10,20))
 print (cube(3))
-print (power(2))
-print (power(2,3))
-print (power(x=3, num=2))
 print (multi_add(4,5,10,4))
